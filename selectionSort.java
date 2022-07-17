5d1be302c9b25b93406a9d014881d31c58a36c19
mehjabinhasanmoni <mehjabinhasanmoni@gmail.com>
2022-07-17 21:19:17 +0600

    Selection Sort

--- selectionSort.java
+++ selectionSort.java
@@ -9,7 +9,7 @@ public class selectionSort {
         public static void main(String[] args) {
             int arr[] = {7,8,3,2,1};
     
-    
+            // Time complexity = O(n^2)
             // selection sort
             for(int i =0;i<arr.length-1; i++){
                 int smallest = i;
